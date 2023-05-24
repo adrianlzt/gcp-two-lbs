2bf47c0210b6e98ae0c0cedc9ad873471a78ab54
Adrian Lopez <adrianlzt@gmail.com>
2023-05-24 08:38:58 +0200

    Trying to create an HTTP and TCP LB pointing to the same instances
    First try. Using different instance groups pointing for each protocol
    (tcp, http) and for each instance.
    Four IGs, two instances.

---
+++ terraform.tf
@@ -0,0 +1,231 @@
+# HTTP-LB
+# forwarding-rule-1 -> proxy1 -> urlmap1 -> backend1  -> instance1-group-http -> instance1
+#                                                     -> instance2-group-http -> instance2
+#
+# TCP-LB
+# forwarding-rule-2 -> backend2  -> instance1-group-tcp -> instance1
+#                                -> instance2-group-tcp -> instance2
+#
+# Error: Error waiting to create RegionBackendService: Error waiting for Creating RegionBackendService: Validation failed for instance 'projects/pruebas-adri-lb/zones/europe-west1-b/instances/instance2': instance may belong to at most one load-balanced instance group.
+
+terraform {
+  backend "local" {
+    path = "terraform.tfstate"
+  }
+}
+
+locals {
+  project_name = "pruebas-adri-lb"
+  region       = "europe-west1"
+  zone         = "europe-west1-b"
+  image        = "rocky-linux-9-optimized-gcp"
+}
+
+provider "google" {
+  project = local.project_name
+  region  = local.region
+  zone    = local.zone
+}
+
+provider "google-beta" {
+  project = local.project_name
+  region  = local.region
+  zone    = local.zone
+}
+
+resource "google_compute_instance" "instance1" {
+  name         = "instance1"
+  machine_type = "e2-micro"
+  zone         = local.zone
+
+  boot_disk {
+    initialize_params {
+      image = local.image
+    }
+  }
+
+  network_interface {
+    network = "default"
+    access_config {
+    }
+  }
+}
+
+resource "google_compute_instance_group" "instance1-group-http" {
+  name        = "instance1-group-http"
+  zone        = local.zone
+
+  instances = [
+    google_compute_instance.instance1.self_link,
+  ]
+
+  named_port {
+    name = "http"
+    port = 80
+  }
+}
+
+resource "google_compute_instance_group" "instance1-group-tcp" {
+  name        = "instance1-group-tcp"
+  zone        = local.zone
+
+  instances = [
+    google_compute_instance.instance1.self_link,
+  ]
+
+  named_port {
+    name = "foo"
+    port = 1000
+  }
+}
+
+resource "google_compute_instance" "instance2" {
+  name         = "instance2"
+  machine_type = "e2-micro"
+  zone         = local.zone
+
+  boot_disk {
+    initialize_params {
+      image = local.image
+    }
+  }
+
+  network_interface {
+    network = "default"
+    access_config {
+    }
+  }
+}
+
+resource "google_compute_instance_group" "instance2-group-http" {
+  name        = "instance2-group-http"
+  zone        = local.zone
+
+  instances = [
+    google_compute_instance.instance2.self_link,
+  ]
+
+  named_port {
+    name = "http"
+    port = 80
+  }
+}
+
+resource "google_compute_instance_group" "instance2-group-tcp" {
+  name        = "instance2-group-tcp"
+  zone        = local.zone
+
+  instances = [
+    google_compute_instance.instance2.self_link,
+  ]
+
+  named_port {
+    name = "foo"
+    port = 1000
+  }
+}
+
+
+resource "google_compute_firewall" "allow-all" {
+  name    = "allow-all"
+  network = "default"
+  allow {
+    protocol = "all"
+  }
+  source_ranges = ["0.0.0.0/0"]
+}
+
+
+# ILB 1 - default groups
+resource "google_compute_subnetwork" "proxy-only" {
+  name          = "proxy-only"
+  ip_cidr_range = "10.20.30.0/24"
+  purpose       = "INTERNAL_HTTPS_LOAD_BALANCER"
+  role          = "ACTIVE"
+  network       = "default"
+}
+
+resource "google_compute_forwarding_rule" "forwarding-rule-1" {
+  name                  = "forwarding-rule-1"
+  region                = local.region
+  load_balancing_scheme = "INTERNAL_MANAGED"
+  target                = google_compute_region_target_http_proxy.proxy1.id
+  port_range            = 80
+}
+
+resource "google_compute_region_target_http_proxy" "proxy1" {
+  depends_on  = [google_compute_subnetwork.proxy-only]
+  name    = "proxy1"
+  url_map = google_compute_region_url_map.urlmap1.id
+}
+
+resource "google_compute_region_url_map" "urlmap1" {
+  region = local.region
+  name   = "urlmap1"
+
+  default_service = google_compute_region_backend_service.backend1.id
+}
+
+resource "google_compute_health_check" "http" {
+  provider = google-beta
+  name     = "http"
+
+  http_health_check {
+    port         = 80
+    request_path = "/"
+  }
+}
+
+resource "google_compute_region_backend_service" "backend1" {
+  name                  = "backend1"
+  region                = local.region
+  protocol              = "HTTP"
+  port_name             = "http"
+  load_balancing_scheme = "INTERNAL_MANAGED"
+  backend {
+    group           = google_compute_instance_group.instance1-group-http.self_link
+    balancing_mode  = "UTILIZATION"
+    capacity_scaler = 1.0
+  }
+  backend {
+    group           = google_compute_instance_group.instance2-group-http.self_link
+    balancing_mode  = "UTILIZATION"
+    capacity_scaler = 1.0
+  }
+  health_checks = [google_compute_health_check.http.self_link]
+}
+
+
+# ILB 2 - default-tcp groups
+resource "google_compute_forwarding_rule" "forwarding-rule-2" {
+  name                  = "forwarding-rule-2"
+  region                = local.region
+  load_balancing_scheme = "INTERNAL"
+  backend_service       = google_compute_region_backend_service.backend2.self_link
+  ports = ["1000"]
+}
+
+resource "google_compute_region_backend_service" "backend2" {
+  name                  = "backend2"
+  region                = local.region
+  protocol              = "TCP"
+  load_balancing_scheme = "INTERNAL"
+  backend {
+    group          = google_compute_instance_group.instance1-group-tcp.id
+    balancing_mode = "CONNECTION"
+  }
+  backend {
+    group          = google_compute_instance_group.instance2-group-tcp.id
+    balancing_mode = "CONNECTION"
+  }
+  health_checks = [google_compute_region_health_check.tcp-1000.self_link]
+}
+
+resource "google_compute_region_health_check" "tcp-1000" {
+  provider = google-beta
+  name     = "tcp-1000"
+
+  tcp_health_check {
+    port = "1000"
+  }
+}
